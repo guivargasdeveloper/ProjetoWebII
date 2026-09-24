da4da007cde19e20ea0e06e81d830496e5994ab8
guivargasdeveloper <guivargas05@gmail.com>
2026-09-24 19:23:54 -0300

    Update login.ts

--- src/controllers/login.ts
+++ src/controllers/login.ts
@@ -1,8 +1,18 @@
 // Importar a biblioteca Express
 import express, {Request, Response} from "express";
 
+// Importar o arquivo com as credenciais do banco de dados
+import {AppDataSource} from "../data-source";
+
 // Criar a aplicação Express
 const router = express.Router();
+
+// Iniciar a conexão com o BD
+AppDataSource.initialize().then(() => {
+    console.log("Conexão com o banco de dados estabelecida com sucesso!");
+}).catch((error)=>{
+    console.log("Erro ao conectar com o banco de dados:", error);
+})
 
 // Criar a rota GET principal
 router.get("/", (req: Request, res: Response) => {
